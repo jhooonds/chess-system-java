efe84398f548887765c75de27ac2803bbcc7f809
Jonatan Silva <41707797+jhooonds@users.noreply.github.com>
2019-12-12 21:34:21 -0300

    first class, position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-System.out.println("Hello word!");
+		Position p1 = new Position(3, 5);
+		
+		System.out.println(p1);
 
 	}
 
